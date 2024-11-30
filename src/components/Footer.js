dc2a4c9b36ae555d32cbba08d092d71b2dfa790c
Caiorossi00 <caio_rossi56@hotmail.com>
2024-11-30 10:48:18 -0300

    ajustes css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "../assets/css/Footer.css";
+import logo from "../assets/images/caiorossi.dev.png";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>Caiorossi.dev</p>
+      <a
+        href="https://instagram.com/caiorossi.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={logo}
+          alt="Logo do Instagram @caiorossi.dev"
+          className="footer-logo"
+        />
+      </a>
     </footer>
   );
 }
